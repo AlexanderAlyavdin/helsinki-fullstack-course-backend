9884b6de702c4a6c65c873e8e558d635f91046a0
Alexander Alyavdin <alexander.alyavdin@gmail.com>
2022-04-05 23:25:03 +0300

    Part 3: Phonebook backend 3.1-3.6

---
+++ index.js
@@ -0,0 +1,95 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+let phonebookData = [
+  {
+    id: 1,
+    name: "Arto Hellas",
+    number: "040-123456",
+  },
+  {
+    id: 2,
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+  },
+  {
+    id: 3,
+    name: "Dan Abramov",
+    number: "12-43-234345",
+  },
+  {
+    id: 4,
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+  },
+];
+
+const getRandomId = () => {
+  return Math.round(Math.random() * 1000000);
+};
+
+app.get("/info", (request, response) => {
+  response.send(`
+  <div>Phonebook has info for ${phonebookData.length} people</div>
+  <br/>  
+  <div>${new Date()}</div>
+  `);
+});
+
+app.get("/api/persons", (request, response) => {
+  response.json(phonebookData);
+});
+
+app.get("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+  const person = phonebookData.find((item) => item.id === id);
+  if (!person) {
+    return response.status(404).end();
+  }
+  response.json(person);
+});
+
+app.delete("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+  phonebookData = phonebookData.filter((person) => person.id !== id);
+
+  response.status(204).end();
+});
+
+app.post("/api/persons", (request, response) => {
+  const id = getRandomId();
+  const { name, number } = request.body;
+  const isNameError = name === undefined;
+  const isNumberError = number === undefined;
+  const connector = isNameError && isNumberError ? " and " : "";
+  const nameError = isNameError ? "name" : "";
+  const numberError = isNumberError ? "number" : "";
+
+  if (isNameError || isNumberError) {
+    return response.status(400).json({
+      error: `need to provide ${nameError}${connector}${numberError} for a person`,
+    });
+  }
+
+  if (phonebookData.find((item) => item.name === name)) {
+    return response.status(400).json({
+      error: `a person ${name} already exists`,
+    });
+  }
+
+  const newPerson = {
+    id,
+    name,
+    number,
+  };
+  phonebookData = [...phonebookData, newPerson];
+
+  response.json(newPerson);
+});
+
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
