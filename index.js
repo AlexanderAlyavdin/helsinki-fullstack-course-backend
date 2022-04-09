21cbac9f4a7351d5ae5d1a5f9c68f72d10b7f8c7
Alexander Alyavdin <alexander.alyavdin@gmail.com>
2022-04-09 12:15:01 +0300

    Part 3: Phonebook backend 3.19-3.21

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms :body")
 );
 
-app.get("/info", (request, response) => {
+app.get("/info", (request, response, next) => {
   Person.find({})
     .then((persons) => {
       response.send(`
@@ -29,7 +29,7 @@ app.get("/info", (request, response) => {
     .catch((error) => next(error));
 });
 
-app.get("/api/persons", (request, response) => {
+app.get("/api/persons", (request, response, next) => {
   Person.find({})
     .then((persons) => {
       response.json(persons);
@@ -37,7 +37,7 @@ app.get("/api/persons", (request, response) => {
     .catch((error) => next(error));
 });
 
-app.get("/api/persons/:id", (request, response) => {
+app.get("/api/persons/:id", (request, response, next) => {
   Person.findById(request.params.id)
     .then((person) => {
       return person ? response.json(person) : response.status(404).end();
@@ -45,7 +45,7 @@ app.get("/api/persons/:id", (request, response) => {
     .catch((error) => next(error));
 });
 
-app.delete("/api/persons/:id", (request, response) => {
+app.delete("/api/persons/:id", (request, response, next) => {
   Person.findByIdAndRemove(request.params.id)
     .then(() => {
       response.status(204).end();
@@ -53,7 +53,7 @@ app.delete("/api/persons/:id", (request, response) => {
     .catch((error) => next(error));
 });
 
-app.post("/api/persons", (request, response) => {
+app.post("/api/persons", (request, response, next) => {
   const { name, number } = request.body;
   const isNameError = name === undefined;
   const isNumberError = number === undefined;
@@ -84,12 +84,18 @@ app.put("/api/persons/:id", (request, response, next) => {
   const body = request.body;
   const { name, number } = body;
 
-  const person = {
-    name,
-    number,
-  };
-
-  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+  Person.findByIdAndUpdate(
+    request.params.id,
+    {
+      name,
+      number,
+    },
+    {
+      new: true,
+      runValidators: true,
+      context: "query",
+    }
+  )
     .then((updatedPerson) => {
       response.json(updatedPerson);
     })
@@ -101,6 +107,8 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === "CastError") {
     return response.status(400).send({ error: "malformatted id" });
+  } else if (error.name === "ValidationError") {
+    return response.status(400).json({ error: error.message });
   }
 
   next(error);
